2ba125d0d3c287e8acc6049e03d63bf4ef1e6c4c
Degamer <olayinkabello962@gmail.com>
2024-10-24 17:23:57 +0100

    please work

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/ui/app-sidebar"
+// import { AppSidebar } from "@/components/ui/app-sidebar"
 
 const inter = Inter({ subsets: ["latin"] });
 
