31657960750e7df7b373a7b94779aea767dcc1da
Degamer <olayinkabello962@gmail.com>
2024-11-10 01:12:39 +0100

    added bitcoin

--- src/app/main/admin/page.tsx
+++ src/app/main/admin/page.tsx
@@ -8,12 +8,17 @@ import Card from "@/components/ui/card"
 import PlaceOrder from "@/components/place-order";
 import Referral from "@/components/referral";
 import { DataTableDemo } from "@/components/ui/table-for-things";
+import { useUserData } from "@/components/store";
 
 export default function Admin() {
+    const allUserData = useUserData( state => state.allUserData)
+    const getAllUserData = useUserData( state => state.getAllUserData)
+    getAllUserData()
     return (
         <div>
             <h2 className="font-bold text-xl mt-2">Hello, User</h2>
             <section>
+                {allUserData.toString()}
                 {/* <Referral /> */}
             </section>
             <DataTableDemo />
--- src/app/main/payment/page.tsx
+++ src/app/main/payment/page.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 const AddFund = () => {
   return (
     <div className="p-6 bg-gray-100">
-      <div className="text-xl font-bold mb-4">Add Fund(USDT)</div>
+      <div className="text-xl font-bold mb-4">Add Fund(Bitcoin)</div>
       <div className="p-4 bg-white border border-gray-300 rounded-md shadow-md">
         <div className="flex items-center mb-4">
           <div className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full">
             <span className="text-xl">$</span>
           </div>
-          <p className="ml-4">Make Payment of <span className="font-semibold">2000$</span> to the address below and click &quot; SEND PROOF OF DEPOSIT&quot;</p>
+          <p className="ml-4">Make Payment<span className="font-semibold"></span> to the address below and click &quot; SEND PROOF OF DEPOSIT&quot;</p>
+          {/* <p className="ml-4">Make Payment of <span className="font-semibold">2000$</span> to the address below and click &quot; SEND PROOF OF DEPOSIT&quot;</p> */}
         </div>
 
         <div className="flex flex-col md:flex-row items-center mb-4">
@@ -23,7 +24,7 @@ const AddFund = () => {
             <input
               type="text"
               readOnly
-              value="TTF5ZaFCHNMMg81PuXjGiim9W6oGAYcLuH"
+              value="1GUbDxT8uLSzj5TopnpvVpGqutfBgg1zRf"
               className="w-full p-2 border border-gray-300 rounded-md focus:outline-none mb-2"
             />
             <button className="bg-gray-300 text-sm px-4 py-1 rounded-md">Copy</button>
@@ -34,7 +35,7 @@ const AddFund = () => {
           <label className="block mb-2 text-gray-600">Upload Proof of Deposit:</label>
           <input type="file" className="block w-full border border-gray-300 rounded-md p-1.5" />
         </div>
-
+        
         <button className="w-full bg-blue-600 text-white py-2 rounded-md font-semibold hover:bg-blue-700">
           SEND PROOF OF DEPOSIT
         </button>
